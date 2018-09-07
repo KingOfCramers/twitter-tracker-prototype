f8ee5890d8c9c291136babaef438b8a6f3c0175b
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-09-06 21:55:42 -0400

    added remove button to edit page

--- src/components/EditStoryPage.js
+++ src/components/EditStoryPage.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { connect } from "react-redux";
 import moment from "moment";
+import { startRemoveStory } from "../actions/stories"
 
 class EditStoryPage extends React.Component {
+
+  onRemove = () => {
+    this.props.removeStory({ id: this.props.story.id });
+    this.props.history.push("/");
+  }
+
   render(props){
     return (
      <div>
       <p>{this.props.story.story}</p>
       <p>{this.props.story.description}</p>
       <p>{moment(this.props.story.dueDate).format("LL")}</p>
+      <button onClick={this.onRemove}>Remove</button>
     </div>
     );
   }
 }
 
-const mapStateToProps = (state, props) => { // we have access to the props as the second argument here.
-  console.log("State is", state, "Props are", props)
-  return {
+const mapStateToProps = (state, props) => ({ // we have access to the props as the second argument here.
     story: state.stories.find((story) => story.id === props.match.params.id) // the props.match.params comes from the Router (it's part of the URL)
-  }
-};
+});
 
-export default connect(mapStateToProps)(EditStoryPage);
+const mapDispatchToProps = (dispatch) => ({
+  removeStory: (story) => dispatch(startRemoveStory(story))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditStoryPage);
